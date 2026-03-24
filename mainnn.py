9b06fdada77bba2e3d65589bb2c127d1a70815a1
ChaithanyaJ <chaithanyajanjam@gmail.com>
2026-03-24 18:53:57 +0530

    Create mainnn.py

---
+++ mainnn.py
@@ -0,0 +1,9 @@
+
+ "sk-xxxxxxxxxxxxxxxxxxxxx"
+
+chaithu
+
+OPENAI_KEY = "sk-abcdefghijklmnopqrstuvwxyz123456"
+AWS_KEY = "AKIA1234567890ABCDEF"
+GITHUB_TOKEN = "ghp_abcdefghijklmnopqrstuvwxyz1234567890"
+STRIPE_KEY = "sk_live_51H8abcXYZ1234567890abcdef"
